29cc01b360501ae1d8cb9da2c7b393c83ea18abe
m-lima <marcelowind@gmail.com>
2016-10-22 13:39:57 +0200

    Simplify profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -23,17 +23,6 @@ function activateVS {
 	}
 }
 
-function activateGit {
-	Remove-Item alias:\git
-	. (Resolve-Path "$env:LOCALAPPDATA\GitHub\shell.ps1")
-	Import-Module $env:github_posh_git\posh-git.psm1
-	$GLOBAL:gitActive = $true
-	Write-Host "Git Command Prompt variables set" -ForegroundColor Yellow
-	if ($args.count 2> $null) {
-		git $args
-	}
-}
-
 function ee {
 	ii .
 }
@@ -64,60 +53,16 @@ function def($word) {
 	Write-Host $out -ForegroundColor Green
 }
 
-<#
 function prompt {
 	$separator = ""
-	Write-Host -NoNewline " π " -BackgroundColor "Black"
-	Write-Host -NoNewline $separator -ForegroundColor "Black" -BackgroundColor "DarkBlue"
-	$location = " " + $(Get-Location).ToString() + " "
-	Write-Host $location.Replace("C:\Users\mflim_000", "~") -NoNewLine -ForegroundColor "Black" -BackgroundColor "DarkBlue"
-
-	if ($GLOBAL:gitActive) {
-		$status = Get-GitStatus
-
-		if ($status) {
-		  if ($status.HasWorking) {
-			$backColor = "DarkYellow"
-			if ($status.HasUntracked) {
-			  $gitLine = "  " + $status.Branch + " ±" + $status.Working.Length + "! "
-			} else {
-			  $gitLine = "  " + $status.Branch + " ±" + $status.Working.Length + " "
-			}
-		  } else {
-			$backColor = "DarkGreen"
-			$gitLine = "  " + $status.Branch + " "
-		  }
-		  Write-Host -NoNewline $separator -ForegroundColor "DarkBlue" -BackgroundColor $backColor
-		  Write-Host -NoNewline $gitLine -ForegroundColor "Black" -BackgroundColor $backColor
-		  Write-Host -NoNewline $separator -ForegroundColor $backColor
-		} else {
-		  Write-Host -NoNewline $separator -ForegroundColor "DarkBlue"
-		}
-	} else {
-		Write-Host -NoNewline $separator -ForegroundColor "DarkBlue"
-	}
-	
-	$GLOBAL:nowPath = (Get-Location).Path
-    if (($nowPath -ne $oldDir) -AND $GLOBAL:addToStack) {
-        $GLOBAL:dirStack.Push($oldDir)
-        $GLOBAL:oldDir = $nowPath
-    }
-    $GLOBAL:AddToStack = $true
-	
-	return " "
-}
-#>
-
-function prompt {
-	$separator = ""
-	Write-Host -NoNewline " π "
+	Write-Host -NoNewline " Њ "
   if ($env:HOME -eq $(Get-Location).Path) {
 	  Write-Host "~" -NoNewLine
   } else {
     Write-Host $(Split-Path $PWD -Leaf) -NoNewLine
   }
 
-	if ($GLOBAL:gitActive) {
+	<#if ($GLOBAL:gitActive) {
 		$status = Get-GitStatus
 
 		if ($status) {
@@ -133,10 +78,10 @@ function prompt {
       Write-Host -NoNewline $separator -BackgroundColor "DarkBlue" -ForegroundColor "Black"
 		  Write-Host -NoNewline $separator -ForegroundColor "DarkBlue"
 		}
-	} else {
+	} else {#>
 		Write-Host -NoNewline $separator -BackgroundColor "DarkBlue" -ForegroundColor "Black"
 		Write-Host -NoNewline $separator -ForegroundColor "DarkBlue"
-	}
+	# }
 	
 	$GLOBAL:nowPath = (Get-Location).Path
     if (($nowPath -ne $oldDir) -AND $GLOBAL:addToStack) {
@@ -159,21 +104,13 @@ Import-Module PowerTab
 
 #Alias
 set-alias gvim "${env:ProgramFiles(x86)}\Vim\vim80\gvim.exe"
-set-alias nano "${env:USERPROFILE}\Bin\Nano\nano-2.4.2-win32\nano.exe"
 set-alias npd "${env:ProgramFiles(x86)}\Notepad++\notepad++.exe"
+set-alias vi "${env:ProgramFiles(x86)}\Vim\vim80\vim.exe"
 
 set-alias whr "where.exe"
 set-alias bd BackOneDir
-set-alias ss c:\cygwin64\bin\ssh.exe
-#set-alias ss C:\Users\mflim_000\AppData\Local\scoop\shims\ssh.exe
-set-alias fd "${env:USERPROFILE}\AppData\Local\GitHub\Portab~1\usr\bin\find.exe"
-set-alias vi "${env:ProgramFiles(x86)}\Vim\vim80\vim.exe"
-#set-alias vi "${env:USERPROFILE}\AppData\Local\GitHub\Portab~1\usr\bin\vim.exe"
+#set-alias fd "${env:USERPROFILE}\AppData\Local\GitHub\Portab~1\usr\bin\find.exe"
 
-set-alias git activateGit
+#set-alias git activateGit
 set-alias cl activateVS
-
-set-alias gurl C:\Users\mflim_000\AppData\Local\scoop\shims\curl.exe
-
-set-alias oct octave-cli
 
