14250cae7fc69f3cbcc3833237870328fcdeee8b
m-lima <marcelowind@gmail.com>
2016-10-22 14:27:49 +0200

    Better profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,30 +1,30 @@
 ﻿function touch {
-	ni -type file $args
+  ni -type file $args
 }
 
 function vd {
-	cd "${env:USERPROFILE}"
+  cd "${env:USERPROFILE}"
 }
 
 function activateVS {
-	Remove-Item alias:\cl
-	#Set environment variables for Visual Studio Command Prompt
-	pushd 'C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC'
-	cmd /c "vcvarsall.bat x64&set" |
-	foreach {
-	  if ($_ -match "=") {
-		$v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
-	  }
-	}
-	popd
-	Write-Host "Visual Studio 2015 Command Prompt variables set" -ForegroundColor Yellow
-	if ($args.count 2> $null) {
-		cl $args
-	}
+  Remove-Item alias:\cl
+  #Set environment variables for Visual Studio Command Prompt
+  pushd 'C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC'
+  cmd /c "vcvarsall.bat x64&set" |
+  foreach {
+    if ($_ -match "=") {
+      $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
+    }
+  }
+  popd
+  Write-Host "Visual Studio 2015 Command Prompt variables set" -ForegroundColor Yellow
+  if ($args.count 2> $null) {
+    cl $args
+  }
 }
 
 function ee {
-	ii .
+  ii .
 }
 
 function octg {
@@ -39,58 +39,52 @@ function cpwd {
 }
 
 function BackOneDir {
-	$GLOBAL:addToStack = $false
-	if ($GLOBAL:dirStack.Count) {
-		$lastDir = $GLOBAL:dirStack.Pop()
-		cd $lastDir
-	}
+  $GLOBAL:addToStack = $false
+  if ($GLOBAL:dirStack.Count) {
+    $lastDir = $GLOBAL:dirStack.Pop()
+    cd $lastDir
+  }
 }
 
 function def($word) {
-	$word = "http://google-dictionary.so8848.com/meaning?word=" + $word
-	$result = Invoke-WebRequest $word
-	$out = $result.AllElements | Where Class -EQ "std" | Select -ExpandProperty innerText
-	Write-Host $out -ForegroundColor Green
+  $word = "http://google-dictionary.so8848.com/meaning?word=" + $word
+  $result = Invoke-WebRequest $word
+  $out = $result.AllElements | Where Class -EQ "std" | Select -ExpandProperty innerText
+  Write-Host $out -ForegroundColor Green
 }
 
 function prompt {
-	$separator = ""
-	Write-Host -NoNewline " Њ "
+  $separator = ""
+  Write-Host -NoNewline " Њ "
   if ($env:HOME -eq $(Get-Location).Path) {
-	  Write-Host "~" -NoNewLine
+    Write-Host "~" -NoNewLine
   } else {
     Write-Host $(Split-Path $PWD -Leaf) -NoNewLine
   }
 
-	<#if ($GLOBAL:gitActive) {
-		$status = Get-GitStatus
-
-		if ($status) {
-      if ($status.HasWorking) {
-        $backColor = "DarkYellow"
-      } else {
-        $backColor = "DarkGreen"
-      }
-
-		  Write-Host -NoNewline $separator -ForegroundColor "Black" -BackgroundColor $backColor
-		  Write-Host -NoNewline $separator -ForegroundColor $backColor
-		} else {
-      Write-Host -NoNewline $separator -BackgroundColor "DarkBlue" -ForegroundColor "Black"
-		  Write-Host -NoNewline $separator -ForegroundColor "DarkBlue"
-		}
-	} else {#>
-		Write-Host -NoNewline $separator -BackgroundColor "DarkBlue" -ForegroundColor "Black"
-		Write-Host -NoNewline $separator -ForegroundColor "DarkBlue"
-	# }
-	
-	$GLOBAL:nowPath = (Get-Location).Path
-    if (($nowPath -ne $oldDir) -AND $GLOBAL:addToStack) {
-        $GLOBAL:dirStack.Push($oldDir)
-        $GLOBAL:oldDir = $nowPath
+  ($isGit = git rev-parse --is-inside-work-tree) | out-null
+  if ($isGit -eq "true") {
+    ($status = git status --porcelain) | out-null
+    if ($status.length -gt 0) {
+      $backColor = "DarkYellow"
+    } else {
+      $backColor = "DarkGreen"
     }
-    $GLOBAL:AddToStack = $true
-	
-	return " "
+    Write-Host -NoNewline $separator -ForegroundColor "Black" -BackgroundColor $backColor
+    Write-Host -NoNewline $separator -ForegroundColor $backColor
+  } else {
+    Write-Host -NoNewline $separator -BackgroundColor "DarkBlue" -ForegroundColor "Black"
+    Write-Host -NoNewline $separator -ForegroundColor "DarkBlue"
+  }
+
+  $GLOBAL:nowPath = (Get-Location).Path
+  if (($nowPath -ne $oldDir) -AND $GLOBAL:addToStack) {
+    $GLOBAL:dirStack.Push($oldDir)
+    $GLOBAL:oldDir = $nowPath
+  }
+  $GLOBAL:AddToStack = $true
+
+  return " "
 }
 
 #BackDir variables
@@ -100,7 +94,7 @@ $GLOBAL:addToStack = $true
 $GLOBAL:gitActive = $false
 
 #Modules
-Import-Module PowerTab
+Import-Module "PowerTab" -ArgumentList "D:\Users\Marcelo\Documents\WindowsPowerShell\PowerTabConfig.xml"
 
 #Alias
 set-alias gvim "${env:ProgramFiles(x86)}\Vim\vim80\gvim.exe"
