1083c64ae132ff37f27c26f45fd61cb5e00ffdb2
m-lima <marcelowind@gmail.com>
2017-04-03 20:35:14 +0200

    BD is recursive on PS?

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -50,9 +50,13 @@ function ppwd {
   cd $clipPath.Substring(1, $clipPath.Length - 3)
 }
 
-function BackOneDir {
+function bd {
+  param(
+    [int]$amount = 1
+  )
+
   $GLOBAL:addToStack = $false
-  if ($GLOBAL:dirStack.Count) {
+  while ($amout -gt 0 && $GLOBAL:dirStack.Count) {
     $lastDir = $GLOBAL:dirStack.Pop()
     cd $lastDir
   }
@@ -173,7 +177,7 @@ set-alias tlt "${env:LOCALAPPDATA}/scoop/shims/telnet.exe"
 set-alias l ls
 
 # Functions
-set-alias bd BackOneDir
+# set-alias bd BackOneDir
 set-alias cl activateVS
 set-alias go activateGO
 
