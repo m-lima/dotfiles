d38c7cfb169bde3c6b8a90d9fc4871153bfc85e0
Marcelo Lima <marcelo@telenordigital.com>
2018-11-07 16:23:21 +0100

    Enabled async completions; Changed keymapping for more vim-like

--- zsh/localConfig.zsh
+++ zsh/localConfig.zsh
@@ -1,11 +1,13 @@
 # Auto Suggestion Config
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=4"
-# ZSH_AUTOSUGGEST_USE_ASYNC="ON"  [Disabled while bug https://github.com/zsh-users/zsh-autosuggestions/issues/241 is active]
+# ZSH_AUTOSUGGEST_STRATEGY=( "history" "completion" )
+ZSH_AUTOSUGGEST_USE_ASYNC="ON" # [Disabled while bug https://github.com/zsh-users/zsh-autosuggestions/issues/241 is active]
 ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE="200"
-bindkey '^J' autosuggest-execute
-bindkey '^N' autosuggest-next
-bindkey '^P' autosuggest-previous
-bindkey '^F' forward-word
+bindkey '^N' autosuggest-execute
+bindkey '^K' autosuggest-next
+bindkey '^J' autosuggest-previous
+bindkey '^L' forward-word
+bindkey '^H' backward-delete-word
 
 # History Substring Config
 setopt HIST_IGNORE_ALL_DUPS
