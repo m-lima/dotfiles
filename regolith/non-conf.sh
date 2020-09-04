43180437f7bb7ad5dfe6654dc3d2cc6eef3d209b
m-lima <marcelowind@gmail.com>
2020-09-04 19:52:29 +0200

    Document some commands to run on the side of the config files

--- regolith/non-conf.sh
+++ regolith/non-conf.sh
@@ -1,2 +1,5 @@
 dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:escape']"
 gsettings set org.gnome.desktop.interface font-name 'Catarell 11'
+
+dconf write /org/gnome/desktop/background/picture-uri 'file:///path/to/file.png'
+dconf write /org/gnome/desktop/background/primary-color "'#18212D'"
