5df01ec50e262df2686d8c09e9460d337418f87e
m-lima <marcelowind@gmail.com>
2020-09-05 12:42:50 +0200

    Theme with gray and orange

--- regolith/non-conf.sh
+++ regolith/non-conf.sh
@@ -1,5 +1,13 @@
+# Replace capslocak with escape
 dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:escape']"
+
+# If change gnome fonts
 gsettings set org.gnome.desktop.interface font-name 'Catarell 11'
 
+# Manually set the background (allows for centered image, for example)
 dconf write /org/gnome/desktop/background/picture-uri 'file:///path/to/file.png'
 dconf write /org/gnome/desktop/background/primary-color "'#18212D'"
+dconf write /org/gnome/desktop/background/picture-options "'centered'"
+
+# Change the system-wide login screen
+vi /usr/share/lightdm/lightdm-gtk-greeter.conf.d/02_regolith.conf
