49f1092d9388dc53dbf53c64f4b2be6b59743850
m-lima <marcelowind@gmail.com>
2021-06-13 10:53:58 +0200

    Update regolith

--- regolith/i3xrocks/custom/checker/build.sh
+++ regolith/i3xrocks/custom/checker/build.sh
@@ -4,3 +4,9 @@ gcc -O3 gpu-usage.c -o gpu-usage -flto -lX11 \
   && sudo chown root:root gpu-usage \
   && sudo chmod 755 gpu-usage \
   && sudo chmod u+s gpu-usage
+
+if [ $? -eq 0 ] && [[ "$1" == "install" ]]
+then
+  sudo rm /usr/share/i3xrocks/gpu-usage
+  sudo ln -s "${PWD}/gpu-usage" /usr/share/i3xrocks/.
+fi
