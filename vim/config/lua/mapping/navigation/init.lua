62e20ef6d8a2f65ba016ddfee34b062238bc1f88
Marcelo Lima <marcelowind@gmail.com>
2022-01-26 00:23:26 +0100

    Update to <cmd>

--- vim/config/lua/mapping/navigation/init.lua
+++ vim/config/lua/mapping/navigation/init.lua
@@ -1,20 +1,20 @@
 ---- Telescope
 
 -- General
-vim.api.nvim_set_keymap('n', '<leader><leader><leader>', ':Telescope resume<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader><leader><leader>', '<cmd>Telescope resume<CR>', { noremap = true, silent = true })
 
 -- Search
-vim.api.nvim_set_keymap('n', '<leader><leader>8', ':Telescope grep_string<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '?', ':Telescope live_grep<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader><leader>8', '<cmd>Telescope grep_string<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '?', '<cmd>Telescope live_grep<CR>', { noremap = true, silent = true })
 
 -- Navigation
-vim.api.nvim_set_keymap('n', '<leader>p', ':Telescope find_files<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>P', ':Telescope find_files no_ignore=true<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader><leader>o', ':Telescope oldfiles<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>b', ':Telescope buffers<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader><c-o>', ':Telescope jumplist<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>m', ':Telescope marks<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>p', '<cmd>Telescope find_files<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>P', '<cmd>Telescope find_files no_ignore=true<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader><leader>o', '<cmd>Telescope oldfiles<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>b', '<cmd>Telescope buffers<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader><c-o>', '<cmd>Telescope jumplist<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>m', '<cmd>Telescope marks<CR>', { noremap = true, silent = true })
 
 ---- Projects
-vim.api.nvim_set_keymap('n', '<leader><leader>p', ':Telescope projects<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader><leader>p', '<cmd>Telescope projects<CR>', { noremap = true, silent = true })
 
