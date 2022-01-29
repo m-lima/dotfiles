be0ec8cfafcda4726fb092a2c2922d12a734e648
Marcelo Lima <marcelowind@gmail.com>
2022-01-29 19:22:25 +0100

    Fix codelens

--- vim/config/lua/config/nvim-lsp-installer.lua
+++ vim/config/lua/config/nvim-lsp-installer.lua
@@ -3,7 +3,7 @@ local lsp_installer = require('nvim-lsp-installer')
 lsp_installer.on_server_ready(
   function(server)
     local opts = {
-      on_attach = function()
+      on_ready = function()
         -- TODO: Update this when the API for autocmd stabilizes
         vim.cmd([[autocmd! BufEnter,CursorHold,CursorHoldI,InsertLeave <buffer> silent! lua vim.lsp.codelens.refresh()]])
       end
