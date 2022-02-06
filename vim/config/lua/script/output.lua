f8950a9b04ad59270237d1f07cbd67c83d55a5e8
Marcelo Lima <marcelowind@gmail.com>
2022-02-06 17:35:49 +0100

    Simple helper scripts

---
+++ vim/config/lua/script/output.lua
@@ -0,0 +1,37 @@
+local function buffer(cmd)
+  local str = no_inspect and cmd or vim.inspect(cmd)
+
+  local lines = {}
+  for line in str:gmatch('([^\n]+)') do
+    table.insert(lines, line)
+  end
+
+  local width = vim.api.nvim_get_option('columns')
+  local height = vim.api.nvim_get_option('lines')
+
+  local win_height = math.ceil(height * 0.8 - 4)
+  local win_width = math.ceil(width * 0.8)
+
+  local row = math.ceil((height - win_height) / 2 - 1)
+  local col = math.ceil((width - win_width) / 2)
+
+  local target = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_buf_set_option(target, 'bufhidden', 'wipe')
+  if not no_inspect then
+    vim.api.nvim_buf_set_option(target, 'filetype', 'lua')
+  end
+  vim.api.nvim_buf_set_lines(target, 0, #lines, false, lines)
+
+  vim.api.nvim_open_win(target, true, {
+    style = 'minimal',
+    relative = 'editor',
+    width = win_width,
+    height = win_height,
+    row = row,
+    col = col,
+  })
+end
+
+return {
+  buffer = buffer
+}
