7c5efda4273b0eb5380ba761736502558c913115
Marcelo Lima <marcelowind@gmail.com>
2022-02-07 18:43:21 +0100

    Add repls (need to check if binaries exist first)

--- vim/config/lua/config/toggleterm/init.lua
+++ vim/config/lua/config/toggleterm/init.lua
@@ -4,10 +4,12 @@ require('toggleterm').setup({
 
 local map = require('script.helper').map
 
-map('n', '<C-q>',      '<cmd>ToggleTerm direction=float<CR>')
-map('t', '<C-q>',      '<cmd>ToggleTerm<CR>')
-map('n', '<leader>q',  ':ToggleTerm direction=horizontal<CR>')
-map('n', '<leader>qq', '<cmd>ToggleTermToggleAll<CR>')
-map('n', '<leader>qn', ':ToggleTerm direction=horizontal<CR>')
-map('n', '<leader>qf', '<cmd>ToggleTerm direction=float<CR>')
-map('n', '<leader>qg', '<cmd>lua require("config.toggleterm.extension.lazygit"):toggle()<CR>')
+map('n', '<C-q>',       '<cmd>ToggleTerm direction=float<CR>')
+map('t', '<C-q>',       '<cmd>ToggleTerm<CR>')
+map('n', '<leader>q',   ':ToggleTerm direction=horizontal<CR>')
+map('n', '<leader>qq',  '<cmd>ToggleTermToggleAll<CR>')
+map('n', '<leader>qn',  ':ToggleTerm direction=horizontal<CR>')
+map('n', '<leader>qf',  '<cmd>ToggleTerm direction=float<CR>')
+map('n', '<leader>qg',  '<cmd>lua require("config.toggleterm.extension.lazygit"):toggle()<CR>')
+map('n', '<leader>qrr', '<cmd>lua require("config.toggleterm.extension.evcxr"):toggle()<CR>')
+map('n', '<leader>qrn', '<cmd>lua require("config.toggleterm.extension.node"):toggle()<CR>')
