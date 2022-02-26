9f519cbc29e58ba3a57afaf3cf9b3c8d252ead00
Marcelo Lima <marcelowind@gmail.com>
2022-02-26 17:57:48 +0100

    Allow changing inlay colors

--- vim/config/lua/plugin/inlay.lua
+++ vim/config/lua/plugin/inlay.lua
@@ -1,9 +1,7 @@
-local M = {}
+local namespace = vim.api.nvim_create_namespace('m-lima.inlay')
+local hl = vim.api.nvim_get_hl_id_by_name('LspCodeLens')
 
--- TODO: Customize the colors
-M.namespace = vim.api.nvim_create_namespace('m-lima.inlay')
-M.hl = vim.api.nvim_get_hl_id_by_name('LspCodeLens')
-M.hints = function()
+local hints = function()
   vim.lsp.buf_request(
     0,
     'rust-analyzer/inlayHints',
@@ -13,7 +11,8 @@ M.hints = function()
         return
       end
 
-      vim.api.nvim_buf_clear_namespace(ctx.bufnr, M.namespace, 0, -1)
+      vim.api.nvim_buf_clear_namespace(ctx.bufnr, namespace, 0, -1)
+
       for _, v in ipairs(res) do
         if v.kind == 'TypeHint' then
           if v.range.start.line == v.range['end'].line then
@@ -22,16 +21,26 @@ M.hints = function()
             local var = string.sub(vim.api.nvim_buf_get_lines(ctx.bufnr, start.line, start.line + 1, false)[1], start.character + 1, finish.character)
             local line = start.line
             local str = var .. ': ' .. v.label
-            vim.api.nvim_buf_set_extmark(ctx.bufnr, M.namespace, line, 0, { virt_text = {{ str, M.hl }}, hl_mode = 'combine' })
+            vim.api.nvim_buf_set_extmark(ctx.bufnr, namespace, line, 0, { virt_text = {{ str, hl }}, hl_mode = 'combine' })
           end
         elseif v.kind == 'ChainingHint' then
           local line = v.range['end'].line
           local str = '‣' .. v.label
-          vim.api.nvim_buf_set_extmark(ctx.bufnr, M.namespace, line, 0, { virt_text = {{ str, M.hl }}, hl_mode = 'combine' })
+          vim.api.nvim_buf_set_extmark(ctx.bufnr, namespace, line, 0, { virt_text = {{ str, hl }}, hl_mode = 'combine' })
         end
       end
     end
   )
 end
 
-return M
+local set_hl_group = function(name)
+  if vim.fn.hlexists(name) ~= 0 then
+    hl = vim.api.nvim_get_hl_id_by_name(name)
+    hints()
+  end
+end
+
+return {
+  hints = hints,
+  set_hl_group = set_hl_group,
+}
