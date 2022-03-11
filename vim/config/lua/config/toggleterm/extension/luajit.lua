8a604f9414acb089ebea80d7667a6df4d2c1f3d7
Marcelo Lima <marcelowind@gmail.com>
2022-03-11 17:35:52 +0100

    Add luajit repl

---
+++ vim/config/lua/config/toggleterm/extension/luajit.lua
@@ -0,0 +1,8 @@
+return require('toggleterm.terminal').Terminal:new({
+  cmd = 'luajit',
+  direction = 'float',
+  hidden = true,
+  on_open = function(term)
+    vim.api.nvim_buf_set_keymap(term.bufnr, 't', '<C-q>', '<cmd>close<CR>', { noremap = true, silent = true })
+  end,
+})
