1c0a2aa73f1e9c828475fc5a5fbd65ea6f976c0b
Marcelo Lima <marcelowind@gmail.com>
2022-03-18 13:55:35 +0100

    Don't preselect anything, please

--- vim/config/lua/config/cmp.lua
+++ vim/config/lua/config/cmp.lua
@@ -2,6 +2,7 @@ local cmp = require('cmp')
 local luasnip = require('luasnip')
 
 cmp.setup({
+  preselect = cmp.PreselectMode.None,
   snippet = {
     expand = function(args)
       luasnip.lsp_expand(args.body)
