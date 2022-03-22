31422b4249297edcb06d6f8b9b5b4173fc1f2514
Marcelo Lima <marcelowind@gmail.com>
2022-03-22 09:40:47 +0100

    Round off dap

--- vim/config/lua/config/dap/adapters.lua
+++ vim/config/lua/config/dap/adapters.lua
@@ -20,11 +20,11 @@ M.codelldb = function()
       stderr:close()
       handle:close()
       if code ~= 0 then
-        print("codelldb exited with code", code)
+        print('codelldb exited with code', code)
       end
     end)
 
-    assert(handle, "Error running codelldb: " .. tostring(pid_or_err))
+    assert(handle, 'Error running codelldb: ' .. tostring(pid_or_err))
     stdout:read_start(function(err, chunk)
       assert(not err, err)
       if chunk then
@@ -39,7 +39,7 @@ M.codelldb = function()
           end)
         else
           vim.schedule(function()
-            require("dap.repl").append(chunk)
+            require('dap.repl').append(chunk)
           end)
         end
       end
@@ -49,102 +49,95 @@ M.codelldb = function()
       assert(not err, err)
       if chunk then
         vim.schedule(function()
-          require("dap.repl").append(chunk)
+          require('dap.repl').append(chunk)
         end)
       end
     end)
   end
 
   dap.configurations.rust = {{
-    name = "Launch",
-    type = "codelldb",
-    request = "launch",
+    name = 'Launch',
+    type = 'codelldb',
+    request = 'launch',
     -- TODO: Grab from rust
     -- TODO: List on telescope
     program = function()
       return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
     end,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = false,
     args = function()
-      return vim.fn.input('Args: ')
+      local str = vim.fn.input('Args: ')
+      local args = {}
+      for a in string.gmatch(str, '([^ ]+)') do
+        table.insert(args, a)
+      end
+      return args
     end,
-    cwd = '${workspaceFolder}',
-    stopOnEntry = false,
-    args = {},
-
-    -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
-    --
-    --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
-    --
-    -- Otherwise you might get the following error:
-    --
-    --    Error on launch: Failed to attach to the target process
-    --
-    -- But you should be aware of the implications:
-    -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
     runInTerminal = false,
   }}
   dap.configurations.c = dap.configurations.rust
   dap.configurations.cpp = dap.configurations.rust
 end
 
-M.lldb = function()
-  dap.adapters.lldb = {
-    name = "lldb",
-    type = 'executable',
-    command = '/opt/homebrew/Cellar/llvm/13.0.1/bin/lldb-vscode',
-    -- type = 'server',
-    -- host = '127.0.0.1',
-    -- port = 58183,
-  }
-  dap.configurations.rust = {{
-    name = "Launch",
-    type = "lldb",
-    request = "launch",
-    -- TODO: Grab from rust
-    -- TODO: List on telescope
-    program = function()
-      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-    end,
-    cwd = '${workspaceFolder}',
-    stopOnEntry = false,
-    args = {},
-
-    -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
-    --
-    --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
-    --
-    -- Otherwise you might get the following error:
-    --
-    --    Error on launch: Failed to attach to the target process
-    --
-    -- But you should be aware of the implications:
-    -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
-    runInTerminal = false,
-  }}
-  dap.configurations.c = dap.configurations.rust
-  dap.configurations.cpp = dap.configurations.rust
-end
-
-M.cpptools = function()
-  dap.adapters.cppdbg = {
-    id = 'cppdbg',
-    type = 'executable',
-    command = '/Users/celo/code/utils/cpptools/extension/debugAdapters/bin/OpenDebugAD7',
-  }
-  dap.configurations.rust = {
-    {
-      name = "Launch file",
-      type = "cppdbg",
-      request = "launch",
-      program = function()
-        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-      end,
-      cwd = '${workspaceFolder}',
-      stopOnEntry = true,
-    },
-  }
-  dap.configurations.c = dap.configurations.rust
-  dap.configurations.cpp = dap.configurations.rust
-end
+-- M.lldb = function()
+--   dap.adapters.lldb = {
+--     name = 'lldb',
+--     type = 'executable',
+--     command = '/opt/homebrew/Cellar/llvm/13.0.1/bin/lldb-vscode',
+--     -- type = 'server',
+--     -- host = '127.0.0.1',
+--     -- port = 58183,
+--   }
+--   dap.configurations.rust = {{
+--     name = 'Launch',
+--     type = 'lldb',
+--     request = 'launch',
+--     -- TODO: Grab from rust
+--     -- TODO: List on telescope
+--     program = function()
+--       return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+--     end,
+--     cwd = '${workspaceFolder}',
+--     stopOnEntry = false,
+--     args = {},
+--
+--     -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+--     --
+--     --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+--     --
+--     -- Otherwise you might get the following error:
+--     --
+--     --    Error on launch: Failed to attach to the target process
+--     --
+--     -- But you should be aware of the implications:
+--     -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+--     runInTerminal = false,
+--   }}
+--   dap.configurations.c = dap.configurations.rust
+--   dap.configurations.cpp = dap.configurations.rust
+-- end
+--
+-- M.cpptools = function()
+--   dap.adapters.cppdbg = {
+--     id = 'cppdbg',
+--     type = 'executable',
+--     command = '/Users/celo/code/utils/cpptools/extension/debugAdapters/bin/OpenDebugAD7',
+--   }
+--   dap.configurations.rust = {
+--     {
+--       name = 'Launch file',
+--       type = 'cppdbg',
+--       request = 'launch',
+--       program = function()
+--         return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+--       end,
+--       cwd = '${workspaceFolder}',
+--       stopOnEntry = true,
+--     },
+--   }
+--   dap.configurations.c = dap.configurations.rust
+--   dap.configurations.cpp = dap.configurations.rust
+-- end
 
 return M
--- vim/config/lua/config/lsp-installer/rust.lua
+++ vim/config/lua/config/lsp-installer/rust.lua
@@ -27,6 +27,47 @@ vim.lsp.commands['rust-analyzer.runSingle'] = function(cmd)
   end
 end
 
+vim.lsp.commands['rust-analyzer.debugSingle'] = function(cmd)
+  local arguments = cmd.arguments[1]
+
+  local cargo_args = arguments.args.cargoArgs
+  table.insert(cargo_args, '--message-format=json')
+  cargo_args = vim.list_extend(cargo_args, arguments.args.cargoExtraArgs)
+
+  require('plenary.job'):new({
+    command = 'cargo',
+    args = cargo_args,
+    cwd = arguments.workspaceRoot,
+    on_exit = function(job, code)
+      if code and code > 0 then
+        vim.schedule(function()
+          vim.notify('An error occured while compiling. Please fix all compilation issues and try again.', vim.log.levels.ERROR)
+        end)
+      end
+
+      vim.schedule(function()
+        for _, value in pairs(job:result()) do
+          local json = vim.fn.json_decode(value)
+          if type(json) == 'table' and json.executable ~= vim.NIL and json.executable ~= nil then
+            require('dap').run({
+              name = 'Rust debug',
+              type = 'codelldb',
+              request = 'launch',
+              program = json.executable,
+              args = arguments.executableArgs or {},
+              cwd = arguments.workspaceRoot,
+              stopOnEntry = false,
+              runInTerminal = false,
+            })
+            break
+          end
+        end
+      end)
+    end,
+  })
+  :start()
+end
+
 local root_dir = function(filename)
   filename = filename or vim.api.nvim_buf_get_name(0)
 
