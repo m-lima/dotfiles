22c3edc5188b6c4617617da6ab7fc8e3a75cb2c7
Marcelo Lima <marcelowind@gmail.com>
2022-04-11 23:16:00 +0200

    Revert adhoc treesitter grammars

--- vim/config/lua/config/nvim-treesitter.lua
+++ vim/config/lua/config/nvim-treesitter.lua
@@ -42,23 +42,3 @@ require('nvim-treesitter.configs').setup({
     },
   },
 })
-
-local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
-parser_config.pest = {
-  install_info = {
-    url = '~/code/ehrm/tree-sitter-pest',
-    files = { 'src/parser.c' },
-    generate_requires_npm = false,
-    requires_generate_from_grammar = false,
-  },
-  filetype = 'pest',
-}
-parser_config.proto = {
-  install_info = {
-    url = '~/code/others/tree-sitter-proto',
-    files = { 'src/parser.c' },
-    generate_requires_npm = false,
-    requires_generate_from_grammar = false,
-  },
-  filetype = 'proto',
-}
