920605196ee0bb7ebb5d954408377b35218bb538
Marcelo Lima <marcelowind@gmail.com>
2022-04-11 13:42:17 +0200

    Add pest and proto (WIP)

--- vim/config/lua/config/nvim-treesitter.lua
+++ vim/config/lua/config/nvim-treesitter.lua
@@ -26,19 +26,39 @@ require('nvim-treesitter.configs').setup({
       goto_next_start = {
         [']m'] = '@function.outer',
         [']]'] = '@class.outer',
-    },
-    goto_next_end = {
-      [']M'] = '@function.outer',
-    [']['] = '@class.outer',
-    },
-    goto_previous_start = {
-      ['[m'] = '@function.outer',
-    ['[['] = '@class.outer',
-    },
-    goto_previous_end = {
-      ['[M'] = '@function.outer',
-    ['[]'] = '@class.outer',
-    },
+      },
+      goto_next_end = {
+        [']M'] = '@function.outer',
+        [']['] = '@class.outer',
+      },
+      goto_previous_start = {
+        ['[m'] = '@function.outer',
+        ['[['] = '@class.outer',
+      },
+      goto_previous_end = {
+        ['[M'] = '@function.outer',
+        ['[]'] = '@class.outer',
+      },
     },
   },
 })
+
+local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+parser_config.pest = {
+  install_info = {
+    url = '~/code/ehrm/tree-sitter-pest',
+    files = { 'src/parser.c' },
+    generate_requires_npm = false,
+    requires_generate_from_grammar = false,
+  },
+  filetype = 'pest',
+}
+parser_config.proto = {
+  install_info = {
+    url = '~/code/others/tree-sitter-proto',
+    files = { 'src/parser.c' },
+    generate_requires_npm = false,
+    requires_generate_from_grammar = false,
+  },
+  filetype = 'proto',
+}
