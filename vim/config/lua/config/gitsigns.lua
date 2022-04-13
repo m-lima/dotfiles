3736ae3a5a03363da7aabfa6a90ce8c2fe311e06
Marcelo Lima <marcelowind@gmail.com>
2022-04-13 13:23:27 +0200

    Use plenary for change_base

--- vim/config/lua/config/gitsigns.lua
+++ vim/config/lua/config/gitsigns.lua
@@ -4,6 +4,7 @@ require('gitsigns').setup({
   },
 })
 
+local Job = require('plenary.job')
 local map = require('script.helper').map
 
 -- Navigation
@@ -11,73 +12,27 @@ map('n', ']g', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", { expr = true })
 map('n', '[g', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", { expr = true })
 
 -- Actions
-map('n', '<leader>gs',         '<cmd>Gitsigns stage_hunk<CR>')
-map('v', '<leader>gs',         '<cmd>Gitsigns stage_hunk<CR>')
-map('n', '<leader>gr',         '<cmd>Gitsigns reset_hunk<CR>')
-map('v', '<leader>gr',         '<cmd>Gitsigns reset_hunk<CR>')
-map('n', '<leader>gS',         '<cmd>Gitsigns stage_buffer<CR>')
-map('n', '<leader>gu',         '<cmd>Gitsigns undo_stage_hunk<CR>')
-map('n', '<leader>gR',         '<cmd>Gitsigns reset_buffer<CR>')
-map('n', '<leader>gp',         '<cmd>Gitsigns preview_hunk<CR>')
-map('n', '<leader>gb',         '<cmd>lua require("gitsigns").blame_line{full=true}<CR>')
-map('n', '<leader>gB',         '<cmd>Gitsigns toggle_current_line_blame<CR>')
-map('n', '<leader>gd',         '<cmd>Gitsigns diffthis<CR>')
-map('n', '<leader>gD',         '<cmd>lua require("gitsigns").diffthis("~")<CR>')
-map('n', '<leader>gm',         '<cmd>lua require("config.gitsigns").change_base()<CR>')
-map('n', '<leader>gM',         '<cmd>Gitsigns reset_base<CR>')
+map('n', '<leader>gs', '<cmd>Gitsigns stage_hunk<CR>')
+map('v', '<leader>gs', '<cmd>Gitsigns stage_hunk<CR>')
+map('n', '<leader>gr', '<cmd>Gitsigns reset_hunk<CR>')
+map('v', '<leader>gr', '<cmd>Gitsigns reset_hunk<CR>')
+map('n', '<leader>gS', '<cmd>Gitsigns stage_buffer<CR>')
+map('n', '<leader>gu', '<cmd>Gitsigns undo_stage_hunk<CR>')
+map('n', '<leader>gR', '<cmd>Gitsigns reset_buffer<CR>')
+map('n', '<leader>gp', '<cmd>Gitsigns preview_hunk<CR>')
+map('n', '<leader>gb', '<cmd>lua require("gitsigns").blame_line{full=true}<CR>')
+map('n', '<leader>gB', '<cmd>Gitsigns toggle_current_line_blame<CR>')
+map('n', '<leader>gd', '<cmd>Gitsigns diffthis<CR>')
+map('n', '<leader>gD', '<cmd>lua require("gitsigns").diffthis("~")<CR>')
+map('n', '<leader>gm', '<cmd>lua require("config.gitsigns").change_base()<CR>')
+map('n', '<leader>gM', '<cmd>Gitsigns reset_base<CR>')
 map('n', '<leader><leader>gd', '<cmd>Gitsigns toggle_deleted<CR>')
 
 -- Text objects
 map('o', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
 map('x', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
 
-local handle_job_data = function(data)
-  if not data then
-    return nil
-  end
-  for i = #data, 1, -1 do
-    if data[i] == '' then
-      table.remove(data, i)
-    end
-  end
-  if #data < 1 then
-    return nil
-  end
-  return data
-end
-
 local change_base = function(base, head)
-  local lines = {}
-  local error = ''
-
-  local on_event = function(_, data, event)
-    if event == 'stdout' then
-      data = handle_job_data(data)
-      if not data then
-        return
-      end
-
-      for i = 1, #data do
-        table.insert(lines, data[i])
-      end
-    elseif event == 'stderr' then
-      data = handle_job_data(data)
-      if not data then
-        return
-      end
-
-      for _, line in ipairs(data) do
-        error = error .. '\n' .. line
-      end
-    elseif event == 'exit' then
-      if #error > 0 then
-        vim.notify('Error during running a job:\n' .. error, vim.log.levels.ERROR)
-      else
-        require('gitsigns').change_base(lines[1])
-      end
-    end
-  end
-
   if not base or type(base) ~= 'string' or #base == 0 then
     base = 'master'
   end
@@ -86,13 +41,21 @@ local change_base = function(base, head)
     head = 'HEAD'
   end
 
-  vim.fn.jobstart('git merge-base ' .. base .. ' ' .. head, {
-    on_stderr = on_event,
-    on_stdout = on_event,
-    on_exit = on_event,
-    stdout_buffered = true,
-    stderr_buffered = true,
-  })
+  Job:new({
+    command = 'git',
+    args = { 'merge-base', base, head },
+    on_exit = vim.schedule_wrap(function(job, status)
+      if status == 0 then
+        require('gitsigns').change_base(job:result()[1])
+      else
+        vim.notify('Error while executing `git merge-base`:', vim.log.levels.ERROR)
+        print('outter')
+        for _, e in ipairs(job:stderr_result()) do
+          vim.notify(e, vim.log.levels.ERROR)
+        end
+      end
+    end),
+  }):start()
 end
 
 return {
