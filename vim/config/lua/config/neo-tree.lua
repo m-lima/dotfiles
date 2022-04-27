01614f8d598aa42b2dbe6985d3c494319121da2e
Marcelo Lima <marcelowind@gmail.com>
2022-04-27 11:38:31 +0200

    Stay within lua

--- vim/config/lua/config/neo-tree.lua
+++ vim/config/lua/config/neo-tree.lua
@@ -23,7 +23,7 @@ local function getTelescopeOpts(state, path)
   }
 end
 
-vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+vim.g.neo_tree_remove_legacy_commands = 1
 
 require('neo-tree').setup({
   close_if_last_window = true,
