47469fdd60ad631b6bd55616092e7a40c36d2781
Marcelo Lima <m-lima@users.noreply.github.com>
2022-06-13 11:06:40 +0200

    Configure signature

--- vim/config/lua/config/signature.lua
+++ vim/config/lua/config/signature.lua
@@ -1,1 +1,9 @@
-require('lsp_signature').setup({})
+require('lsp_signature').setup({
+  max_height = 40,
+  max_width = 100,
+
+  hint_enable = false,
+  hint_prefix = '',
+
+  toggle_key = '<C-x>',
+})
