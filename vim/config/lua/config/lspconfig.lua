4174b42f9fe013f3e920f5f88c85892ae7e75349
Marcelo Lima <m-lima@users.noreply.github.com>
2022-06-25 16:07:15 +0200

    Show signature_help

--- vim/config/lua/config/lspconfig.lua
+++ vim/config/lua/config/lspconfig.lua
@@ -1,11 +1,14 @@
 local map = require('script.helper').map
 
-map('n', ']e',                vim.diagnostic.goto_next)
-map('n', '[e',                vim.diagnostic.goto_prev)
-map('n', '<leader>d',         vim.lsp.buf.hover)
-map('n', '<leader><leader>r', vim.lsp.buf.rename)
-map('n', '<leader>r',         vim.lsp.codelens.run)
-map('n', '<leader>a',         vim.lsp.buf.code_action)
+map('n',        ']e',                vim.diagnostic.goto_next)
+map('n',        '[e',                vim.diagnostic.goto_prev)
+map('n',        '<leader>d',         vim.lsp.buf.hover)
+map({'n', 's'}, '<leader>D',         vim.lsp.buf.signature_help)
+map('i',        '<C-d>',             vim.lsp.buf.signature_help)
+map('n',        '<leader><leader>r', vim.lsp.buf.rename)
+map('n',        '<leader>r',         vim.lsp.codelens.run)
+map('n',        '<leader>a',         vim.lsp.buf.code_action)
+map('n',        '<leader>a',         vim.lsp.buf.code_action)
 
 vim.diagnostic.config({
   severity_sort = true,
