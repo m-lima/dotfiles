b36f7d001a1464cb495ceb61d7135dc65d3334bf
Marcelo Lima <m-lima@users.noreply.github.com>
2022-07-28 22:30:17 +0200

    Bring nali-autosuggestion closer to default bindings

--- zsh/config/plugins.zsh
+++ zsh/config/plugins.zsh
@@ -77,7 +77,7 @@ esac
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=4"
 ZSH_AUTOSUGGEST_USE_ASYNC="ON"
 ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE="200"
-bindkey '^N' autosuggest-execute
+bindkey '^Y' autosuggest-execute
 bindkey '^K' autosuggest-next
 bindkey '^J' autosuggest-previous
 bindkey '^L' forward-word
