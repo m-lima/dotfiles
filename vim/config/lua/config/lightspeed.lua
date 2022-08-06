85686f00f0175c1971056a5741d7dcdca0c7b3fe
Marcelo Lima <m-lima@users.noreply.github.com>
2022-08-06 11:13:00 +0200

    Disable lightspeed f/t/,/;

---
+++ vim/config/lua/config/lightspeed.lua
@@ -0,0 +1,12 @@
+vim.g.lightspeed_no_default_keymaps = true
+
+local map = require('script.helper').map
+
+map({ 'n', 'x' }, 's', '<Plug>Lightspeed_s')
+map({ 'n', 'x' }, 'S', '<Plug>Lightspeed_S')
+map('o', 'z', '<Plug>Lightspeed_s')
+map('o', 'Z', '<Plug>Lightspeed_S')
+map('o', 'x', '<Plug>Lightspeed_x')
+map('o', 'X', '<Plug>Lightspeed_X')
+map('n', 'gs', '<Plug>Lightspeed_gs')
+map('n', 'gS', '<Plug>Lightspeed_gS')
