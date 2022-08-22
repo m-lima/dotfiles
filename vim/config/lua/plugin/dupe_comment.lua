da5142d922620cb3bd0afbae67aacf0f576992ac
Marcelo Lima <m-lima@users.noreply.github.com>
2022-08-22 11:05:40 +0200

    Update dupe_comment to new api

--- vim/config/lua/plugin/dupe_comment.lua
+++ vim/config/lua/plugin/dupe_comment.lua
@@ -1,8 +1,8 @@
 -- Duplicate and comment
 local map = require('util').map
 
-map('v', 'gp',  [[:copy '><CR>'>+1<cmd>lua require('Comment.api').comment_linewise_op('v')<CR>]])
-map('n', 'gpp', '<cmd>.copy .<CR>k<cmd>lua require("Comment.api").comment_current_linewise()<CR>j')
+map('v', 'gp',  [[:copy '><CR>'>+1<cmd>lua require('Comment.api').comment.linewise('v')<CR>]])
+map('n', 'gpp', '<cmd>.copy .<CR>k<cmd>lua require("Comment.api").comment.linewise.current()<CR>j')
 map('n', 'gp',  [[<cmd>lua vim.api.nvim_set_option('operatorfunc', "v:lua.require'plugin.dupe_comment'.dupe")<CR>g@]])
 
 return {
@@ -15,6 +15,6 @@ return {
     local range = { vim.api.nvim_buf_get_mark(0, '[')[1], vim.api.nvim_buf_get_mark(0, ']')[1] }
     local lines = vim.api.nvim_buf_get_lines(0, range[1] - 1, range[2], false)
     vim.api.nvim_put(lines, 'l', false, true)
-    require('Comment.api').comment_linewise_op('n')
+    require('Comment.api').comment.linewise('n')
   end,
 }
