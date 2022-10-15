823c05279fe610f90fa03ba7209c8b267b5146ae
Marcelo Lima <m-lima@users.noreply.github.com>
2022-10-15 18:52:50 +0200

    Upgrade lsp-installer

--- vim/config/lua/config/lsp-installer/init.lua
+++ vim/config/lua/config/lsp-installer/init.lua
@@ -1,4 +1,4 @@
-local cmp_capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local cmp_capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 vim.api.nvim_create_user_command(
   'Slsp',
