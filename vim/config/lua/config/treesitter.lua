4ba4b73f2e4417b6229857a2438531ecd97fd059
Marcelo Lima <m-lima@users.noreply.github.com>
2022-10-24 12:56:24 +0200

    Add guard on phpdoc for M1

--- vim/config/lua/config/treesitter.lua
+++ vim/config/lua/config/treesitter.lua
@@ -1,5 +1,12 @@
+-- TODO: Remove this once fixed
+local ignore_install = {}
+if jit.os == 'OSX' and jit.arch == 'arm64' then
+  ignore_install = { 'phpdoc' }
+end
+
 require('nvim-treesitter.configs').setup({
   ensure_installed = 'all',
+  ignore_install = ignore_install,
   highlight = {
     enable = true
   },
