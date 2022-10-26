9c1b4f57bc2d6c64de59996eb04564b543e77198
Marcelo Lima <m-lima@users.noreply.github.com>
2022-10-26 11:20:11 +0200

    Recursive search for lsp override

---
+++ vim/config/lua/.vim/lspconfig.lua
@@ -0,0 +1,13 @@
+return {
+  Lua = {
+    diagnostics = {
+      globals = { 'vim' },
+    },
+    workspace = {
+      library = vim.api.nvim_get_runtime_file('', true),
+    },
+    telemetry = {
+      enable = false,
+    },
+  }
+}
--- vim/config/lua/config/lsp-installer/init.lua
+++ vim/config/lua/config/lsp-installer/init.lua
@@ -130,6 +130,23 @@ local make_on_attach = function(opts)
   end
 end
 
+local path_separator = '/'
+if vim.fn.has('win32') == 1 or vim.fn.has('win32unix') == 1 then
+  path_separator = '\\'
+end
+local lspconfig_file = path_separator .. '.vim' .. path_separator .. 'lspconfig.lua'
+
+local search_for_config = function(path)
+  while path ~= path_separator and #path > 0 do
+    path = string.gsub(path, '^(.*)' .. path_separator .. '.+$', '%1')
+    local maybe_config = path .. lspconfig_file
+    local ok, cfg = pcall(dofile, maybe_config)
+    if ok then
+      return cfg, maybe_config
+    end
+  end
+end
+
 require('nvim-lsp-installer').on_server_ready(
   function(server)
     local on_attach_opts = {
@@ -149,13 +166,12 @@ require('nvim-lsp-installer').on_server_ready(
     local opts = {
       capabilities = cmp_capabilities,
       on_init = function(client)
-        local root = client.config.root_dir or vim.fn.getcwd()
-        if root then
-          local ok, cfg = pcall(dofile, root .. '/.vim/lspconfig.lua')
-          if ok then
-            client.config.settings = vim.tbl_deep_extend('force', client.config.settings, cfg)
-            return true
-          end
+        local path = vim.fn.expand('%:p')
+        local cfg, path = search_for_config(path)
+        if cfg then
+          vim.notify('Loaded override from ' .. path)
+          client.config.settings = vim.tbl_deep_extend('force', client.config.settings, cfg)
+          return true
         end
       end
     }
@@ -168,22 +184,10 @@ require('nvim-lsp-installer').on_server_ready(
       -- TODO: PR rust-tools with new inlays
       -- TODO: PR rust-tools with module popup
     elseif server.name == 'sumneko_lua' then
-      -- TODO: Do this only if working with vim files
-      -- TODO: Not quite working.. The global is ok, but the inspection into library is not
       opts.settings = {
         Lua = {
           runtime = {
             version = 'LuaJIT',
-            path = vim.split(package.path, ';'),
-          },
-          diagnostics = {
-            globals = { 'vim' },
-          },
-          workspace = {
-            library = {
-              [vim.env.VIMRUNTIME .. '/lua'] = true,
-              [vim.env.VIMRUNTIME .. '/lua/vim/lsp'] = true,
-            },
           },
           telemetry = {
             enable = false,
