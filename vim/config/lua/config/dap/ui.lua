ae16755a1ff350349cd61b9924862cc202dd8a07
Marcelo Lima <m-lima@users.noreply.github.com>
2023-04-18 10:02:15 +0200

    Dont autoclose dapui

--- vim/config/lua/config/dap/ui.lua
+++ vim/config/lua/config/dap/ui.lua
@@ -14,12 +14,12 @@ end
 dap.listeners.after.event_initialized['dapui_config'] = function()
   dapui().open()
 end
-dap.listeners.before.event_terminated['dapui_config'] = function()
-  dapui().close()
-end
-dap.listeners.before.event_exited['dapui_config'] = function()
-  dapui().close()
-end
+-- dap.listeners.before.event_terminated['dapui_config'] = function()
+--   dapui().close()
+-- end
+-- dap.listeners.before.event_exited['dapui_config'] = function()
+--   dapui().close()
+-- end
 
 vim.api.nvim_create_user_command(
   'DapToggle',
