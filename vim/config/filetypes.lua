d30ce8182d61d20d0a16e825d87185f54f934a5c
Marcelo Lima <m-lima@users.noreply.github.com>
2023-09-01 12:10:57 +0200

    Allow comment not being installed

--- vim/config/filetypes.lua
+++ vim/config/filetypes.lua
@@ -1,12 +1,18 @@
+local ok, comment = pcall(require, 'Comment.ft')
+local set_comment = function() end
+if ok then
+  set_comment = comment.set
+end
+
 vim.filetype.add({
   extension = {
     slint = function(path, bufnr)
-      require('Comment.ft').set('slint', { '//%s', '/*%s*/' })
+      set_comment('slint', { '//%s', '/*%s*/' })
       vim.bo[bufnr].commentstring = '//%s'
       return 'slint'
     end,
     qml = function(path, bufnr)
-      require('Comment.ft').set('qml', { '//%s', '/*%s*/' })
+      set_comment('qml', { '//%s', '/*%s*/' })
       vim.bo[bufnr].commentstring = '//%s'
       return 'qml'
     end,
