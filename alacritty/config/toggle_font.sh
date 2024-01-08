c14ea95c8603230d523dec227fd8034dff067d32
Marcelo Lima <m-lima@users.noreply.github.com>
2024-01-08 11:15:06 +0100

    Update alacritty config

--- alacritty/config/toggle_font.sh
+++ alacritty/config/toggle_font.sh
@@ -1,14 +1,14 @@
-if grep '^  size: 11.0 #FONT_SIZE_MARKER$' "${HOME}/.config/alacritty/transient.yml" &> /dev/null
+if grep '^size = 11.0 #FONT_SIZE_MARKER$' "${HOME}/.config/alacritty/transient.toml" &> /dev/null
 then
-  cat > "${HOME}/.config/alacritty/transient.yml" <<EOF
-font:
-  size: 14.5 #FONT_SIZE_MARKER
+  cat > "${HOME}/.config/alacritty/transient.toml" <<EOF
+[font]
+size = 14.5 #FONT_SIZE_MARKER
 EOF
 else
-  cat > "${HOME}/.config/alacritty/transient.yml" <<EOF
-font:
-  size: 11.0 #FONT_SIZE_MARKER
+  cat > "${HOME}/.config/alacritty/transient.toml" <<EOF
+[font]
+size = 11.0 #FONT_SIZE_MARKER
 EOF
 fi
 
-touch "${HOME}/.config/alacritty/alacritty.yml"
+touch "${HOME}/.config/alacritty/alacritty.toml"
