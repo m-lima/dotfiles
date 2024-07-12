c5876a6e282a5b95fc3a6881425c867cbfd34358
Marcelo Lima <m-lima@users.noreply.github.com>
2024-07-12 15:17:54 +0200

    Support scratches on condensing tmux windows

--- tmux/script/condense_windows.sh
+++ tmux/script/condense_windows.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+windows=$(tmux list-windows -F '#I:#W')
+scratch=$(grep '^[0-9]*:scratch$' <<<"${windows}" | cut -d ':' -f 1)
+
+if [ -n "${scratch}" ]; then
+  curr=$(tmux display-message -p '#I')
+  last=$(( $(tail -1 <<<"${windows}" | cut -d ':' -f 1) + 1 ))
+  if (( scratch != curr)); then
+    background='-d'
+  fi
+  tmux move-window -t "${last}" -s "${scratch}" $background
+fi
+
 windows=( $(tmux list-windows -F '#I') )
 curr=$(tmux display-message -p '#I')
 expected=1
