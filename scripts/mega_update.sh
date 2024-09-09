09997859d4537ac74d086c05e6967b44659bf021
Marcelo Lima <m-lima@users.noreply.github.com>
2024-09-09 19:24:55 +0200

    Full update

---
+++ scripts/mega_update.sh
@@ -0,0 +1,132 @@
+#!/bin/sh
+
+function prompt_question {
+  printf "[34m${1}"
+  if [[ "${2}" == "n" ]]; then
+    printf " [37m[y/N][m "
+  else
+    printf " [37m[Y/n][m "
+  fi
+
+  read input
+  case ${input} in
+    [Yy] ) true ;;
+    [Nn] ) false ;;
+    * ) [[ "${2}" != "n" ]] ;;
+  esac
+}
+
+function update_brew {
+  if prompt_question "Update brew?"; then
+    brew update
+  fi
+
+  outdated="$(brew outdated -v | grep -v '\[pinned')"
+  if [ -n "${outdated}" ]; then
+    echo '[33m::: Brew outdated output :::[m'
+    echo "${outdated}"
+    echo '[33m:::[m'
+
+    if prompt_question "Update brew packages?"; then
+      brew upgrade
+    fi
+  fi
+}
+
+function update_rust {
+  if prompt_question "Update rust?"; then
+    rustup update
+  fi
+
+  outdated="$(cargo install --list | grep '^[^ ]' | cut -d' ' -f1)"
+  echo '[33m::: Cargo list output :::[m'
+  echo "${outdated}"
+  echo '[33m:::[m'
+
+  if prompt_question "Update cargo crates?"; then
+    cargo install ${outdated}
+  fi
+}
+
+function update_zgen {
+  if prompt_question "Update zgen?"; then
+    zgen update
+  fi
+}
+
+function update_npm {
+  outdated="$(npm -g ls -p | grep node_modules | sed 's~.*/~~g')"
+  echo '[33m::: NPM list output :::[m'
+  echo "${outdated}"
+  echo '[33m:::[m'
+
+  if prompt_question "Update NPM?"; then
+    npm -g i ${outdated}
+  fi
+}
+
+function update_nvim {
+  if prompt_question "Update VimPlug?"; then
+    nvim -c 'PlugUpdate' -c 'PlugUpgrade' -c 'qa!'
+  fi
+
+  if prompt_question "Update Mason?"; then
+    nvim --headless -c 'lua
+    local registry = require("mason-registry")
+
+    print("Updating registry..")
+    registry.update(
+      function(success)
+        if not success then
+          vim.schedule(
+            function()
+              print(" ERROR\n")
+              vim.cmd("qa!")
+            end
+          )
+          return
+        end
+        print(" OK\n")
+
+        for _, pkg in ipairs(registry.get_installed_packages()) do
+          pkg:check_new_version(
+            function(new_available, version)
+              if new_available then
+                print("Updating " .. pkg .. "..")
+                pkg:install():on(
+                  "closed",
+                  function()
+                    print(" OK\n")
+                  end
+                )
+              end
+            end
+          )
+        end
+
+        vim.schedule(function() vim.cmd("qa!") end)
+      end
+    )'
+  fi
+}
+
+
+if command -v brew &> /dev/null; then
+  update_brew
+fi
+
+if command -v cargo &> /dev/null; then
+  update_rust
+fi
+
+if command -v zgen &> /dev/null; then
+  update_zgen
+fi
+
+if command -v npm &> /dev/null; then
+  update_npm
+fi
+
+if command -v nvim &> /dev/null; then
+  update_nvim
+fi
