9658522270c58b3a0a2809199c1b2577f15b40f0
Marcelo Lima <m-lima@users.noreply.github.com>
2024-09-25 12:36:39 +0200

    Use fd to feed fzf

--- zsh/config/plugins.zsh
+++ zsh/config/plugins.zsh
@@ -74,7 +74,20 @@ case "${local_pluginManager}" in
 esac
 
 # FZF
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+if [ -f ~/.fzf.zsh ]; then
+  source ~/.fzf.zsh
+  if which -p fdfind &> /dev/null; then
+    export FZF_DEFAULT_COMMAND='fdfind --type file --follow --hidden --exclude .git'
+    _fzf_compgen_path() {
+      echo "${1}"
+      command fdfind "${1}" --follow --hidden --exclude .git 2> /dev/null
+    }
+    _fzf_compgen_dir() {
+      echo "${1}"
+      command fdfind "${1}" --type file --follow --hidden --exclude .git 2> /dev/null
+    }
+  fi
+fi
 
 # Auto Suggestion config
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=4"
