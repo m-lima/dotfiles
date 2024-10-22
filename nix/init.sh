edb8dc395896a0af9034ffa2fede4043b9c6cc16
Marcelo Lima <m-lima@users.noreply.github.com>
2024-10-22 22:05:00 +0200

    Maki init more modular

--- nix/init.sh
+++ nix/init.sh
@@ -12,27 +12,48 @@ fi
 
 host=${1}
 
-echo -n "[33mWARNING!![m This will format the disk. Proceed? [y/N] "
-read input
-case "${input}" in
-  [Yy] ) ;;
-  * ) exit ;;
+function format {
+  echo -n "[33mWARNING!![m This will format the disk. Proceed? [y/N] "
+  read input
+  case "${input}" in
+    [Yy] ) ;;
+    * ) exit ;;
+  esac
+
+  echo "[34mnix --experimental-features 'nix-command flakes' run github:nix-community/disko/latest -- --mode disko '${base}/hosts/${host}/disko.nix'[m"
+  nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode disko "${base}/hosts/${host}/disko.nix"
+}
+
+function mkpass {
+  mkdir /mnt/persist/secrets
+  echo "[34mSetting password for root[m"
+  mkpasswd > /mnt/persist/secrets/root.passwordFile
+  echo "[34mSetting password for celo[m"
+  mkpasswd > /mnt/persist/secrets/celo.passwordFile
+}
+
+function prepare_persist {
+  echo "[34mCreating dotfiles in persist[m"
+  cp -a ${base}/.. /mnt/persist/.
+  mkdir /mnt/persist/etc
+  ln -s /persist/dotfiles/nix /mnt/persist/etc/nixos
+}
+
+function install {
+  nixos-install --flake "${base}#${host}"
+}
+
+case "${2}" in
+  "all")
+    format && mkpass && prepare_persist && install
+    ;;
+  "mount")
+    mount
+    ;;
+  "mkpass")
+    mkpass
+    ;;
+  "install")
+    install
+    ;;
 esac
-
-set -e
-
-echo "[34mnix --experimental-features 'nix-command flakes' run github:nix-community/disko/latest -- --mode disko '${base}/hosts/${host}/disko.nix'[m"
-nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode disko "${base}/hosts/${host}/disko.nix"
-
-mkdir /mnt/persist/secrets
-echo "[34mSetting password for root[m"
-mkpasswd > /mnt/persist/secrets/root.passwordFile
-echo "[34mSetting password for celo[m"
-mkpasswd > /mnt/persist/secrets/celo.passwordFile
-
-echo "[34mCreating dotfiles in persist[m"
-cp -a ${base}/.. /mnt/persist/.
-mkdir /mnt/persist/etc
-ln -s /persist/dotfiles/nix /mnt/persist/etc/nixos
-
-nixos-install --flake "${base}#${host}"
