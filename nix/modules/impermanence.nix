5b3a9975de41e7bd62e71f1ea8bc1637b8ef144e
Marcelo Lima <m-lima@users.noreply.github.com>
2024-10-24 16:59:16 +0200

    Make ssh host fingerprint persistent

--- nix/modules/impermanence.nix
+++ nix/modules/impermanence.nix
@@ -57,7 +57,14 @@ in {
       ];
       files = [
         "/etc/machine-id"
-      ];
+      ] ++ (
+        if config.modules.ssh.enable then [
+          "/etc/ssh/ssh_host_rsa_key"
+          "/etc/ssh/ssh_host_rsa_key.pub"
+          "/etc/ssh/ssh_host_ed25519_key"
+          "/etc/ssh/ssh_host_ed25519_key.pub"
+        ] else []
+      )
     };
   };
 }
