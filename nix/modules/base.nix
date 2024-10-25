868a3d1c2806343fb6ed10e7a53e387aa3bb1266
Marcelo Lima <m-lima@users.noreply.github.com>
2024-10-25 15:21:25 +0200

    Reduce the root programs to bare minimum

--- nix/modules/base.nix
+++ nix/modules/base.nix
@@ -61,62 +61,77 @@
   # To search for available packages:
   # $ nix search nixpkgs wget
   environment.systemPackages = with pkgs; [
-    bat
     curl
-    delta
-    fd
-    fzf
-    git
     neovim
-    ripgrep
-    # TODO: Makes little sense to be global
-    # TODO: Persist
-    zoxide
-    # TODO: Persist history
-    zsh
   ];
 
-  # TODO: This is a smell.. Should go into HM
-  environment.etc = with builtins; {
-    gitignore.text = readFile ../../git/config/ignore;
-    gitconfig.text = ''
-      [core]
-      	excludesfile = /etc/gitignore
-      ''
-      + readFile ../../git/config/gitconfig
-      + readFile ../../git/config/delta;
-  };
+  # environment.systemPackages = with pkgs; [
+  #   bat
+  #   curl
+  #   delta
+  #   fd
+  #   fzf
+  #   git
+  #   neovim
+  #   ripgrep
+  #   zoxide
+  #   zsh
+  # ];
 
-  # TODO: Any guy here should go into HM
+  # # TODO: This is a smell.. Should go into HM
+  # environment.etc = with builtins; {
+  #   gitignore.text = readFile ../../git/config/ignore;
+  #   gitconfig.text = ''
+  #     [core]
+  #     	excludesfile = /etc/gitignore
+  #     ''
+  #     + readFile ../../git/config/gitconfig
+  #     + readFile ../../git/config/delta;
+  # };
+
+  # # TODO: Any guy here should go into HM
+  # programs = {
+  #   fzf = {
+  #     keybindings = true;
+  #     fuzzyCompletion = true;
+  #   };
+  #   # # TODO: Neovim!!
+  #   # neovim = {
+  #   # };
+  #   # TODO: nali-autosuggestions
+  #   # TODO: simpalt
+  #   # TODO: syntax highlight
+  #   zsh = {
+  #     enable = true;
+  #     histSize = 100000;
+  #     enableLsColors = false;
+  #     shellAliases = {};
+  #     interactiveShellInit = with builtins; ''''
+  #       + readFile ../../zsh/config/base/colors.zsh
+  #       + readFile ../../zsh/config/base/completion.zsh
+  #       + readFile ../../zsh/config/base/history.zsh
+  #       + readFile ../../zsh/config/base/keys.zsh
+  #       + readFile ../../zsh/config/base/misc.zsh
+  #       + readFile ../../zsh/config/programs/bat.zsh
+  #       + readFile ../../zsh/config/programs/fzf_fd.zsh
+  #       + readFile ../../zsh/config/programs/git.zsh
+  #       + readFile ../../zsh/config/programs/ls.zsh
+  #       + readFile ../../zsh/config/programs/nvim.zsh
+  #       + readFile ../../zsh/config/programs/rg.zsh
+  #       + readFile ../../zsh/config/programs/zoxide.zsh;
+  #   };
+  # };
+
   programs = {
-    fzf = {
-      keybindings = true;
-      fuzzyCompletion = true;
-    };
-    # # TODO: Neovim!!
-    # neovim = {
-    # };
-    # TODO: nali-autosuggestions
-    # TODO: simpalt
-    # TODO: syntax highlight
-    zsh = {
+    neovim = {
       enable = true;
-      histSize = 100000;
-      enableLsColors = false;
-      shellAliases = {};
-      interactiveShellInit = with builtins; ''''
-        + readFile ../../zsh/config/base/colors.zsh
-        + readFile ../../zsh/config/base/completion.zsh
-        + readFile ../../zsh/config/base/history.zsh
-        + readFile ../../zsh/config/base/keys.zsh
-        + readFile ../../zsh/config/base/misc.zsh
-        + readFile ../../zsh/config/programs/bat.zsh
-        + readFile ../../zsh/config/programs/fzf_fd.zsh
-        + readFile ../../zsh/config/programs/git.zsh
-        + readFile ../../zsh/config/programs/ls.zsh
-        + readFile ../../zsh/config/programs/nvim.zsh
-        + readFile ../../zsh/config/programs/rg.zsh
-        + readFile ../../zsh/config/programs/zoxide.zsh;
+      viAlias = true;
+      defaultEditor = true;
+      configure = {
+        customRC = with builtins; ''''
+          + readFile ../../vim/config/options.vim
+          + readFile ../../vim/config/mapping.vim;
+      };
     };
   };
 
@@ -124,11 +139,9 @@
     mutableUsers = false;
     users = {
       root = {
-        shell = pkgs.zsh;
         hashedPasswordFile = "/persist/secrets/root.passwordFile";
       };
       ${userName} = {
-        shell = pkgs.zsh;
         isNormalUser = true;
         hashedPasswordFile = "/persist/secrets/${userName}.passwordFile";
         extraGroups = [ "wheel" ];
