1b2f99e69b695aaf22a29d121e1a52470ed422df
Marcelo Lima <m-lima@users.noreply.github.com>
2024-11-10 22:21:16 +0100

    Start configuring hyprland

--- nix/home/ui/programs/hyprland.nix
+++ nix/home/ui/programs/hyprland.nix
@@ -10,4 +10,17 @@ let
   cfg = sysconfig.modules.ui.programs.hyprland;
 in util.mkIfUi sysconfig cfg.enable {
   home.packages = with pkgs; lib.mkAfter [ alacritty ];
+
+  wayland.windowManager.hyprland = {
+    enable = true;
+
+    settings = {
+      "$terminal" = lib.mkIf config.home.ui.programs.alacritty.enable "alacritty";
+
+      input = {
+        kb_layout = "us";
+        kb_options = "caps:swapescape";
+      };
+    };
+  };
 }
