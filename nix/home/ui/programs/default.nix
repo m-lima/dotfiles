ec66b6f9dce7311ad01646633325bd8491d236f9
Marcelo Lima <m-lima@users.noreply.github.com>
2024-11-10 22:37:36 +0100

    Install required dependencies

--- nix/home/ui/programs/default.nix
+++ nix/home/ui/programs/default.nix
@@ -1,6 +1,5 @@
 {
   imports = [
-    ./alacritty.nix
     ./hyprland.nix
   ];
 
--- nix/home/ui/programs/hyprland.nix
+++ nix/home/ui/programs/hyprland.nix
@@ -9,18 +9,37 @@
 let
   cfg = sysconfig.modules.ui.programs.hyprland;
 in util.mkIfUi sysconfig cfg.enable {
-  home.packages = with pkgs; lib.mkAfter [ alacritty ];
+  home.packages = with pkgs; lib.mkAfter [
+    alacritty
+    dolphin
+    bemenu
+  ];
 
   wayland.windowManager.hyprland = {
     enable = true;
 
     settings = {
-      "$terminal" = lib.mkIf config.home.ui.programs.alacritty.enable "alacritty";
+      "$terminal" = "alacritty";
+      "$fileManager" = "dolphin";
+      "$launcher" = "bemenu";
 
       input = {
         kb_layout = "us";
         kb_options = "caps:swapescape";
       };
+
+      general = {
+        gaps_in = 4;
+        gaps_out = 8;
+      };
+
+      bind = [
+        "SUPER SHIFT, Q, exit"
+        "SUPER, C, killactive"
+        "SUPER, E, exec, $fileManager"
+        "SUPER, SPACE, exec, $launcher"
+        "SUPER, RETURN, exec, $terminal"
+      ];
     };
   };
 }
