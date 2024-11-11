0125eda61713f32c64a7e548892b0bbbba9097be
Marcelo Lima <m-lima@users.noreply.github.com>
2024-11-11 10:44:04 +0100

    Add SDDM

--- nix/modules/ui/programs/hyprland.nix
+++ nix/modules/ui/programs/hyprland.nix
@@ -10,12 +10,19 @@ in {
   options = {
     modules.ui.programs.hyprland = {
       enable = util.mkDisableOption "hyprland";
+      sddm = {
+        enable = util.mkDisableOption "SDDM";
+      };
     };
   };
 
   config = util.mkIfUi config cfg.enable {
     programs.hyprland = {
       enable = true;
+    };
+    services.displayManager.sddm = lib.mkIf cfg.sddm.enable {
+      enable = true;
+      wayland.enable = true;
     };
   };
 }
