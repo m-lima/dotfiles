ccb249ba9f29a51af491410adb00e1d981882abe
Marcelo Lima <m-lima@users.noreply.github.com>
2024-11-12 22:24:48 +0100

    Allow recursive util call

--- nix/util/default.nix
+++ nix/util/default.nix
@@ -1,7 +1,8 @@
 {
   lib,
   ...
-}: {
+}:
+let
   mkDisableOption = name: lib.mkOption {
       default = true;
       description = "Whether to enable ${name}.";
@@ -11,10 +12,19 @@
 
   mkIfUi = config: condition: lib.mkIf (config.modules.ui.enable && condition);
 
+  mkIfUiSimple = config: mkIfUi config true;
+
   mkColorOption = name: default: lib.mkOption {
     type = lib.types.nonEmptyStr;
     description = "Color in hex for `${name}`";
     default = default;
     example = "ffa500";
   };
+in {
+  inherit
+    mkDisableOption
+    mkIfUi
+    mkIfUiSimple
+    mkColorOption
+  ;
 }
