715e1baa5f16a629bfd1343635352d406fdb2c08
Marcelo Lima <m-lima@users.noreply.github.com>
2024-11-12 18:56:38 +0100

    Add firefox (crude)

---
+++ nix/home/ui/programs/firefox.nix
@@ -0,0 +1,30 @@
+{
+  pkgs,
+  lib,
+  config,
+  sysconfig,
+  util,
+  ...
+}:
+let
+  cfg = config.home.ui.programs.firefox;
+in {
+  options = {
+    home.ui.programs.firefox = {
+      enable = util.mkDisableOption "firefox";
+    };
+  };
+
+  config = util.mkIfUi sysconfig cfg.enable {
+    home.packages = with pkgs; lib.mkAfter [
+      firefox
+    ];
+
+    programs = {
+      firefox = {
+        enable = true;
+      };
+    };
+  };
+}
+
