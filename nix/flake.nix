cd6542a8ab5e230653097a96f10697a33d180799
Marcelo Lima <m-lima@users.noreply.github.com>
2024-11-13 10:01:19 +0100

    Reduce imports

--- nix/flake.nix
+++ nix/flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     disko = {
       url = "github:nix-community/disko";
@@ -20,8 +20,16 @@
       url = "gitlab:Zhaith-Izaliel/sddm-sugar-candy-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    # TODO: Repackage this better. Expose a module
     simpalt = {
       url = "github:m-lima/simpalt-rs";
+      inputs.cargo2nix.follows = "cargo2nix";
+    };
+
+    # Transient
+    cargo2nix = {
+      url = "github:cargo2nix/cargo2nix/release-0.11.0";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
