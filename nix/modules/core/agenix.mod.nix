d0bb3c25884325b8b3614ce252a551708f624b54
Marcelo Lima <m-lima@users.noreply.github.com>
2024-12-05 13:22:35 +0100

    Small agenix fixes

--- nix/modules/core/agenix.mod.nix
+++ nix/modules/core/agenix.mod.nix
@@ -9,14 +9,14 @@ path:
 }:
 let
   cfg = util.getOptions path config;
-  agenix = inputs.agenix-rekey.packages."${pkgs.system}";
+  agenix = inputs.agenix-rekey.packages.${pkgs.system};
 in
 {
   options = util.mkOptions path {
     pubkey = lib.mkOption {
       type = lib.types.coercedTo lib.types.path (
         x: if builtins.isPath x then builtins.readFile x else x
-      ) lib.types.str;
+      ) lib.types.singleLineStr;
       default = ../../hosts/${config.celo.modules.core.hostName}/ssh.key.pub;
       example = "ssh-rsa AAAAB3NzaC1yc2etc/etc/etcjwrsh8e596z6J0l7 example@host";
     };
