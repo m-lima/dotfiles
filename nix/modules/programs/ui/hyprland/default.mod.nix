17fc8626b6f1c2dfeb62407315a42b7323d02edc
Marcelo Lima <m-lima@users.noreply.github.com>
2024-12-08 21:55:55 +0100

    Control media with playerctl

--- nix/modules/programs/ui/hyprland/default.mod.nix
+++ nix/modules/programs/ui/hyprland/default.mod.nix
@@ -190,6 +190,9 @@ in
             ",XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
             ",XF86AudioLowerVolume, exec, wpctl set-volume      @DEFAULT_AUDIO_SINK@ 5%-"
             ",XF86AudioMute,        exec, wpctl set-mute        @DEFAULT_AUDIO_SINK@ toggle"
+            ",XF86AudioPlay,        exec, playerctl -p playerctld play-pause"
+            ",XF86AudioPrev,        exec, playerctl -p playerctld previous"
+            ",XF86AudioNext,        exec, playerctl -p playerctld next"
           ];
 
           workspace = [
