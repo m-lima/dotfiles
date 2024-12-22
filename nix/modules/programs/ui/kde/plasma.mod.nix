36b17212635b9abf62a64fcc1853d7483bdb7a3b
Marcelo Lima <m-lima@users.noreply.github.com>
2024-12-22 14:32:38 +0100

    Test configuring the taskbar

--- nix/modules/programs/ui/kde/plasma.mod.nix
+++ nix/modules/programs/ui/kde/plasma.mod.nix
@@ -36,6 +36,55 @@ in
         session.sessionRestore = {
           restoreOpenApplicationsOnLogin = "startWithEmptySession";
         };
+
+        panels = [
+          {
+            floating = true;
+            widgets = [
+              {
+                kickoff = {
+                  icon = "nix-snowflake-white";
+                };
+              }
+              "org.kde.plasma.pager"
+              {
+                iconTasks = {
+                  launchers = [
+                    "preferred://browser"
+                    "applications:Alacritty.desktop"
+                    "applications:spotify.desktop"
+                    "preferred://filemanager"
+                    "applications:systemsettings.desktop"
+                  ];
+                };
+              }
+              "org.kde.plasma.marginsseparator"
+              {
+                systemTray = {
+                  items = {
+                    hidden = [
+                      "Nextcloud"
+                      "org.kde.kscreen"
+                      "org.kde.plasma.battery"
+                      "org.kde.plasma.bluetooth"
+                      "org.kde.plasma.brightness"
+                      "org.kde.plasma.cameraindicator"
+                      "org.kde.plasma.clipboard"
+                      "org.kde.plasma.devicenotifier"
+                      "org.kde.plasma.keyboardindicator"
+                      "org.kde.plasma.keyboardlayout"
+                      "org.kde.plasma.manage-inputmethod"
+                      "org.kde.plasma.mediacontroller"
+                      "org.kde.plasma.notifications"
+                      "org.kde.plasma.volume"
+                    ];
+                  };
+                };
+              }
+              "org.kde.plasma.digitalclock"
+            ];
+          }
+        ];
       };
     };
 
