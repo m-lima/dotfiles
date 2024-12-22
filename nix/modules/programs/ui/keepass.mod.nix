952fdcfc40b21a101348fa060b9378da8898f523
Marcelo Lima <m-lima@users.noreply.github.com>
2024-12-22 15:27:51 +0100

    Add keepass

--- nix/modules/programs/ui/keepass.mod.nix
+++ nix/modules/programs/ui/keepass.mod.nix
@@ -8,26 +8,19 @@ path:
 }:
 let
   cfg = util.getOptions path config;
-  hyprCfg = config.celo.modules.programs.ui.hyprland;
 in
 {
   options = util.mkOptionsEnable path;
 
   config = util.enforceHome path config cfg.enable {
     home-manager = {
-      home.packages = with pkgs; [ spotify ];
-
-      wayland.windowManager.hyprland = lib.mkIf hyprCfg.enable {
-        settings = {
-          "$player" = "spotify";
-        };
-      };
+      home.packages = with pkgs; [ keepassxc ];
     };
 
     environment.persistence = util.withImpermanence config {
       home.directories = [
-        ".config/spotify"
-        ".cache/spotify"
+        ".cache/keepassxc"
+        ".config/keepassxc"
       ];
     };
   };
