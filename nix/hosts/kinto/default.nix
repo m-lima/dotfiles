2f5b0f9ae3e8451e259b6ddb35796eee2c2a8c70
Marcelo Lima <m-lima@users.noreply.github.com>
2025-01-23 19:39:06 +0100

    Add kinto ports

--- nix/hosts/kinto/default.nix
+++ nix/hosts/kinto/default.nix
@@ -1,34 +1,45 @@
 {
   system = "x86_64-linux";
-  module = {
-    imports = [ ./hardware-configuration.nix ];
+  module =
+    {
+      config,
+      util,
+      ...
+    }:
+    {
+      imports = [ ./hardware-configuration.nix ];
 
-    celo = {
-      profiles = {
-        minimal.enable = true;
-      };
-
-      modules = {
-        core = {
-          disko = {
-            device = "/dev/sda";
-            legacy = true;
-            # luks = true;
-            swap = "8G";
-          };
-          nixos = {
-            timeZone = "Europe/Amsterdam";
-          };
-          user = {
-            userName = "kinto";
-          };
+      celo = {
+        profiles = {
+          minimal.enable = true;
         };
-        programs = {
-          simpalt = {
-            symbol = "ɳ";
+
+        modules = {
+          core = {
+            disko = {
+              device = "/dev/sda";
+              legacy = true;
+              # luks = true;
+              swap = "8G";
+            };
+            nixos = {
+              timeZone = "Europe/Amsterdam";
+            };
+            user = {
+              userName = "kinto";
+            };
+          };
+          services = {
+            ssh = {
+              ports = util.rageSecret config ./secrets/services/ssh/ports.age;
+            };
+          };
+          programs = {
+            simpalt = {
+              symbol = "ɳ";
+            };
           };
         };
       };
     };
-  };
 }
