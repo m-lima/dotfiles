8ee9dcd78ed4c923383a4ca72dce8f8e1c2b9ae0
Marcelo Lima <m-lima@users.noreply.github.com>
2025-04-07 22:19:33 +0200

    Use skull for reals!!!!

--- nix/modules/programs/skull.mod.nix
+++ nix/modules/programs/skull.mod.nix
@@ -8,11 +8,20 @@ path:
 }:
 let
   cfg = util.getOptions path config;
+  host = config.celo.host.id;
 in
 {
   options = util.mkOptionsEnable path;
 
   config = util.enforceHome path config cfg.enable {
+    home-manager = {
+      home.packages = map (
+        url:
+        (builtins.getFlake "${url}?ref=master&rev=cb3ab9eb59d18baa564850e4087feb7b2c4d147d")
+        .outputs.packages.${pkgs.system}.default
+      ) (util.rageSecret config ../../hosts/${host}/secrets/programs/skull/url.age);
+    };
+
     environment.persistence = util.withImpermanence config {
       home.directories = [ ".local/share/Skull" ];
     };
