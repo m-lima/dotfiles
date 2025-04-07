e5569cca38517207eb8225cc41af0ef93dae5eb2
Marcelo Lima <m-lima@users.noreply.github.com>
2025-04-07 17:12:51 +0200

    Simplify td script

--- nix/modules/programs/core/nali.mod.nix
+++ nix/modules/programs/core/nali.mod.nix
@@ -120,7 +120,7 @@ in
           message = "nali enabled without zsh";
         }
         {
-          assertion = cfg.entries != {} -> celo.core.user.home.enable;
+          assertion = cfg.entries != { } -> celo.core.user.home.enable;
           message = "nali entries defined without home-manager";
         }
       ];
@@ -129,70 +129,49 @@ in
         zsh.interactiveShellInit = script;
       };
 
-      home-manager = util.withHome config (
-        if cfg.entries == { } then
-          {
-            programs = {
-              zsh.initExtra = util.extractCompdef script;
-            };
-          }
-        else
-          let
-            entries = pkgs.writeTextDir "td/config" (
-              lib.concatStrings (lib.mapAttrsToList (k: v: "${k}:${v}\n") cfg.entries)
-            );
-          in
-          {
-            home.packages = [ entries ];
-            programs = {
-              zsh.initExtra =
-                (util.extractCompdef script)
-                + ''
+      home-manager =
+        let
+          td = lib.optionalString (cfg.entries != { }) ''
+            function td {
+              if [ "$1" ]
+              then
+                case "$1" in
+                  ${lib.concatStringsSep "\n" (
+                    lib.mapAttrsToList (k: v: ''${k}) eval cd "${v}/$2"; return ;;'') cfg.entries
+                  )}
+                esac
 
-                  function td {
-                    if [ "$1" ]
-                    then
-                      while read line
-                      do
-                        local entry="''${line%%:*}"
-                        if [[ "$entry" == "$1" ]]
-                        then
-                          local dir="''${line#*:}"
-                          eval cd "$dir/$2"
-                          return
-                        fi
-                      done < "${entries}/td/config"
-                      echo Entry not found
-                      return -1
-                    else
-                      cd ~
-                    fi
-                  }
+                echo Entry not found
+                return -1
+              else
+                cd ~
+              fi
+            }
 
-                  function _td {
-                    if (( CURRENT == 2 ))
-                    then
-                      local list=($(cat "${entries}/td/config"))
-                      _describe 'targets' list
+            function _td {
+              if (( CURRENT == 2 ))
+              then
+                local list=( ${
+                  lib.concatStringsSep " " (lib.mapAttrsToList (k: v: ''"${k}:${v}"'') cfg.entries)
+                } )
+                _describe 'targets' list
+              elif (( CURRENT == 3 ))
+              then
+                case "$words[2]" in
+                  ${lib.concatStringsSep "\n" (
+                    lib.mapAttrsToList (k: v: ''${k}) eval _path_files -W "${v}" -/; return ;;'') cfg.entries
+                  )}
+                esac
+              fi
+            }
 
-                    elif (( CURRENT == 3 ))
-                    then
-
-                      while read line
-                      do
-                        if [[ "$(echo $line | cut -d : -f 1)" == "$words[2]" ]]
-                        then
-                          eval _path_files -W $(echo $line | cut -d : -f 2) -/
-                          return
-                        fi
-                      done < "${entries}/td/config"
-                    fi
-                  }
-
-                  compdef _td td
-                '';
-            };
-          }
-      );
+            compdef _td td
+          '';
+        in
+        util.withHome config {
+          programs = {
+            zsh.initExtra = (util.extractCompdef script) + "\n${td}";
+          };
+        };
     };
 }
