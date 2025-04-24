c647ef53f4771093b037749813793ba52d8d7b16
Marcelo Lima <m-lima@users.noreply.github.com>
2025-04-24 20:27:47 +0200

    Add zz

--- zsh/config/programs/zoxide.zsh
+++ zsh/config/programs/zoxide.zsh
@@ -1,1 +1,48 @@
 source <(zoxide init zsh)
+
+function zz {
+  if [ "$#" -eq 0 ]; then
+    cd ~
+    return
+  fi
+
+  local entries=$(zoxide query --list --exclude "${PWD}" "$@")
+
+  if [  -z "$entries" ]; then
+    echo "No matches" >&2
+    return 1
+  fi
+
+  if [[ "$entries" =~ $'\n' ]]; then
+    local result=$(fzf <<<"$entries") && cd "$result"
+  else
+    cd "$entries"
+  fi
+}
+
+function _zz {
+  if (( CURRENT == 2 )); then
+    local entries=$(zoxide query --list --exclude "${PWD}" "${words[2]}")
+
+    if [  -z "$entries" ]; then
+      return
+    fi
+
+    if [[ "$entries" =~ $'\n' ]]; then
+      local result=$(fzf <<<"$entries")
+      if [ -n "$result" ]; then
+        compadd -U -Q -- "$result"
+      fi
+    else
+      compadd -U -Q -- "$entries"
+    fi
+  elif (( CURRENT == 3 )); then
+    if [ -d "${words[2]}" ]; then
+      _path_files -W "${words[2]}" -/
+    else
+      zle -M "Not resolved for '${words[2]}'"
+    fi
+  fi
+}
+
+compdef _zz zz
