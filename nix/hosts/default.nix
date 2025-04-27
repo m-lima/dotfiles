7662ec9f1980f8a8c6aed5d631ec137511c8a7aa
Marcelo Lima <m-lima@users.noreply.github.com>
2025-04-27 13:41:36 +0200

    Document host/default.nix file

--- nix/hosts/default.nix
+++ nix/hosts/default.nix
@@ -1,5 +1,24 @@
 let
+  # Load all hosts, with the filenames as ID
+  # Excludes this file and loads the directories as hosts
   listHosts = removeAttrs (builtins.readDir ./.) [ "default.nix" ];
+
+  # Imports the host and calls `mapHost` passing the ID as parameter
+  loadHost = id: mapHost (import ./${id}) id;
+
+  # Takes in the imported host, sets up the mandatory modules (more below),
+  # And sets a config entry with the ID field.
+  #
+  # Returns the following:
+  # {
+  #   system = <The system type>
+  #   hostModules = <Configuration for the host>
+  # }
+  #
+  # Modules:
+  # module - What comes from the imported host file
+  # hostModule - From below, essentially adds the ID option
+  # setupRagenix - Prepares ragenix
   mapHost =
     { system, module }:
     id: {
@@ -11,7 +30,6 @@ let
         { celo.host.id = id; }
       ];
     };
-  loadHost = id: mapHost (import ./${id}) id;
   hostModule =
     { lib, ... }:
     {
@@ -32,4 +50,6 @@ let
       ragenix.key = lib.mkDefault (builtins.head config.age.identityPaths);
     };
 in
+# For each host directory, call the passed in `mkHost` with the
+# enriched host definition (as defined above)
 mkHost: builtins.mapAttrs (id: _: mkHost (loadHost id)) listHosts
