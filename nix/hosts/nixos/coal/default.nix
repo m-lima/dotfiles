bfa10688948f664a43f7d7f6c86ea014ecdb7551
Marcelo Lima <m-lima@users.noreply.github.com>
2025-07-20 15:17:50 +0200

    Set static ip

--- nix/hosts/nixos/coal/default.nix
+++ nix/hosts/nixos/coal/default.nix
@@ -1,81 +1,105 @@
 {
   system = "x86_64-linux";
-  module = {
-    imports = [ ./hardware-configuration.nix ];
+  module =
+    {
+      lib,
+      config,
+      util,
+      ...
+    }:
+    {
+      imports = [ ./hardware-configuration.nix ];
 
-    celo = {
-      profiles = {
-        system.enable = true;
-        base.enable = true;
-        core.enable = true;
-        creation.enable = true;
-        dev.enable = true;
-        disko.enable = true;
-        nextcloud.enable = true;
-        ui.enable = true;
+      celo = {
+        profiles = {
+          system.enable = true;
+          base.enable = true;
+          core.enable = true;
+          creation.enable = true;
+          dev.enable = true;
+          disko.enable = true;
+          nextcloud.enable = true;
+          ui.enable = true;
+        };
+
+        modules = {
+          core = {
+            agenix = {
+              identityPath = "/persist/etc/ssh/ssh_host_ed25519_key";
+            };
+            disko = {
+              device = "/dev/nvme0n1";
+              luks = true;
+              swap = "8G";
+            };
+            dropbear.enable = true;
+            system = {
+              timeZone = "Europe/Amsterdam";
+              stateVersion = "24.05";
+            };
+          };
+          hardware = {
+            bluetooth.enable = true;
+            sound = {
+              enable = true;
+              persist = true;
+            };
+            wifi.enable = true;
+          };
+          services = {
+            ssh.enable = true;
+            mdns.enable = true;
+          };
+          programs = {
+            git = {
+              overrides = {
+                "~/code/cog" = "cog.age";
+              };
+            };
+            nali = {
+              entries = {
+                cd = "~/code";
+                nx = "~/code/dotfiles/nix";
+                cg = "~/code/cog";
+              };
+            };
+            playerctl.enable = true;
+            simpalt = {
+              symbol = "₵";
+            };
+            skull.enable = true;
+            ui = {
+              alacritty = {
+                tmuxStart = true;
+              };
+              firefox = {
+                multiAccount = true;
+              };
+              kde = {
+                enable = true;
+                insomnia = true;
+              };
+              slack.enable = true;
+            };
+          };
+        };
       };
 
-      modules = {
-        core = {
-          agenix = {
-            identityPath = "/persist/etc/ssh/ssh_host_ed25519_key";
-          };
-          disko = {
-            device = "/dev/nvme0n1";
-            luks = true;
-            swap = "8G";
-          };
-          dropbear.enable = true;
-          system = {
-            timeZone = "Europe/Amsterdam";
-            stateVersion = "24.05";
-          };
+      networking = {
+        interfaces.wlp3s0 = {
+          ipv4.addresses = [
+            {
+              address = "10.0.0.10";
+              prefixLength = 24;
+            }
+          ];
+          useDHCP = false;
         };
-        hardware = {
-          bluetooth.enable = true;
-          sound = {
-            enable = true;
-            persist = true;
-          };
-          wifi.enable = true;
-        };
-        services = {
-          ssh.enable = true;
-          mdns.enable = true;
-        };
-        programs = {
-          git = {
-            overrides = {
-              "~/code/cog" = "cog.age";
-            };
-          };
-          nali = {
-            entries = {
-              cd = "~/code";
-              nx = "~/code/dotfiles/nix";
-              cg = "~/code/cog";
-            };
-          };
-          playerctl.enable = true;
-          simpalt = {
-            symbol = "₵";
-          };
-          skull.enable = true;
-          ui = {
-            alacritty = {
-              tmuxStart = true;
-            };
-            firefox = {
-              multiAccount = true;
-            };
-            kde = {
-              enable = true;
-              insomnia = true;
-            };
-            slack.enable = true;
-          };
-        };
+        defaultGateway = "10.0.0.1";
+        nameservers = [ "10.0.0.1" ];
+        firewall.allowedTCPPorts = builtins.head (
+          (util.rageSecret config ./_secrets/core/networking/firewall.rage) ++ [ ]
+        );
       };
     };
-  };
 }
