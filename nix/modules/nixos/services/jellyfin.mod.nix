b2a4b55060f942f16fa361636eea4bf9bf85f25c
Marcelo Lima <m-lima@users.noreply.github.com>
2025-10-26 17:55:42 +0100

    Remove uneeded packages

--- nix/modules/nixos/services/jellyfin.mod.nix
+++ nix/modules/nixos/services/jellyfin.mod.nix
@@ -44,12 +44,6 @@ in
       };
     };
 
-    environment.systemPackages = [
-      pkgs.jellyfin
-      pkgs.jellyfin-web
-      pkgs.jellyfin-ffmpeg
-    ];
-
     services = {
       jellyfin = {
         enable = true;
