42ff45f3b2e5d736efd16d1d0c4ccce3804dbe80
Marcelo Lima <m-lima@users.noreply.github.com>
2026-02-26 14:56:25 +0100

    Update LSCOLORS

--- zsh/config/base/colors.zsh
+++ zsh/config/base/colors.zsh
@@ -1,8 +1,8 @@
 alias ls='ls --color=tty'
 
 # Colors for ls
-export LSCOLORS='Gxfxcxdxbxagafxbabacad'
-export LS_COLORS='di=1;36:ln=35:so=32:pi=33:ex=31:bd=30;46:cd=30;45:su=0;41:sg=30;41:tw=30;42:ow=30;43'
+export LSCOLORS='ExGxFxdaCxDaDahbadacec'
+export LS_COLORS='rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=00:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32'
 
 # Match the completion colors to LS_COLORS
 zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
