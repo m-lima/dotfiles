4d29765717dc05db13079b72cf73010695309025
Marcelo Lima <m-lima@users.noreply.github.com>
2026-02-28 21:48:11 +0100

    Update skull noquick

--- nix/flake.nix
+++ nix/flake.nix
@@ -150,7 +150,7 @@
       };
     };
     skull = {
-      url = "github:m-lima/skull?ref=v0.11.3";
+      url = "github:m-lima/skull?ref=v0.11.4";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         crane.follows = "crane";
