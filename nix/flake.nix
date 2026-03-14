5d8984a815e7ccb80ed6ab83a9e5a007fc967887
Marcelo Lima <m-lima@users.noreply.github.com>
2026-03-14 12:35:10 +0100

    Update to skull with charts

--- nix/flake.nix
+++ nix/flake.nix
@@ -150,7 +150,7 @@
       };
     };
     skull = {
-      url = "github:m-lima/skull?ref=v0.11.5";
+      url = "github:m-lima/skull?ref=v0.11.6";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         crane.follows = "crane";
