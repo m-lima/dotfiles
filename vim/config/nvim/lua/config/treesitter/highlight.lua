78eb3bac77c5fe251005f235788d7ceb5f2d2bbd
Marcelo Lima <m-lima@users.noreply.github.com>
2026-06-10 22:43:57 +0200

    Avoid focus on notify for treesitter unknown grammar

--- vim/config/nvim/lua/config/treesitter/highlight.lua
+++ vim/config/nvim/lua/config/treesitter/highlight.lua
@@ -3,11 +3,7 @@ vim.api.nvim_create_autocmd('FileType', {
   pattern = '*',
   callback = function(args)
     if vim.bo[args.buf].buftype == "" then
-      local ok, err = pcall(vim.treesitter.start, args.buf)
-      if not ok then
-        local msg = string.format("Could not start treesitter: %s", vim.inspect(err))
-        vim.notify(msg, vim.log.levels.WARN, { title = "Treesitter" })
-      end
+      pcall(vim.treesitter.start, args.buf)
     end
   end,
 })
